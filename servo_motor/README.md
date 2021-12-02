acc4a3d0e2fc75ea9e7608cbcf0c591c0c0dbb42
Suraphop <suraphop.bunsawat@gmail.com>
2021-12-02 10:00:19 +0700

    servo

---
+++ servo_motor/README.md
@@ -0,0 +1,11 @@
+## Requirement 
+- for arm64 (jetson boards, raspberry pi,window,linux)
+
+### How to use
+#### simple running servo motor
+1. run servo.py 
+```
+python3 servo.py
+```
+
+
---
+++ servo_motor/servo.py
@@ -0,0 +1,44 @@
+'''
+A simple servo motor running
+can rotate angle by a toggle
+@auther suraphop bunsawat
+
+changable 1 point
+1. define pin number (pin = 13)
+'''
+
+#import tools
+from tkinter import *
+import RPi.GPIO as GPIO
+import time
+
+#define pin number
+pin = 13
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.OUT)
+
+#setup pwm
+pwm = GPIO.PWM(pin, 100)
+pwm.start(5)
+
+class App:
+    '''control servo motor'''	
+    def __init__(self, master):
+        '''creale toggle'''
+        frame = Frame(master)
+        frame.pack()
+        scale = Scale(frame, from_=0, to=180, 
+              orient=HORIZONTAL, command=self.update)
+        scale.grid(row=0)
+
+    def update(self, angle):
+        '''change angle'''
+        duty = float(angle) / 10.0 + 2.5
+        pwm.ChangeDutyCycle(duty)
+
+#running program
+root = Tk()
+root.wm_title('Servo Control')
+app = App(root)
+root.geometry("200x50+0+0")
+root.mainloop()
